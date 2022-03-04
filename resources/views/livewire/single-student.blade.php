668e1bc02180b4741b14678ac0fdc3d77ba980f3
jltkd <jl@turnkeydigital.com>
2022-03-04 15:17:05 -0600

    Removed Tooltips

--- resources/views/livewire/single-student.blade.php
+++ resources/views/livewire/single-student.blade.php
@@ -18,69 +18,37 @@
                 </div>
                 <div class="flex">
                     @if($student->first_writing)
-                        <div x-data="{ tooltip: false }" class="relative inline-flex">
-                            <button x-on:mouseover="tooltip = true" x-on:mouseleave="tooltip = false" class="flex-shrink-0 pr-2">
+                        <div class="relative inline-flex">
+                            <button class="flex-shrink-0 pr-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-green-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
                                 </svg>
                             </button>
-                            <div class="relative" x-cloak x-show.transition.origin.top="tooltip">
-                                <div class="absolute top-0 z-10 w-56 p-2 -mt-1 text-sm leading-tight text-white transform -translate-x-1/2 -translate-y-full bg-orange-500 rounded-lg shadow-lg">
-                                    Writing Sample One  {{ $student->first_writing ? 'Complete' : '' }}
-                                </div>
-                                <svg class="absolute z-10 w-6 h-6 text-orange-500 transform -translate-x-12 -translate-y-3 fill-current stroke-current" width="8" height="8">
-                                    <rect x="12" y="-10" width="8" height="8" transform="rotate(45)" />
-                                </svg>
-                            </div>
                         </div>
                     @else
-                        <div x-data="{ tooltip: false }" class="relative inline-flex">
-                            <button onclick="Livewire.emit('openModal', 'writing-sample-modal', {{ json_encode(['student' => $student->id]) }})" x-on:mouseover="tooltip = true" x-on:mouseleave="tooltip = false" class="flex-shrink-0 pr-2">
+                        <div class="relative inline-flex">
+                            <button class="flex-shrink-0 pr-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6  {{ $student->first_writing ? 'text-green-500' : 'text-gray-400' }}" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                                 </svg>
                             </button>
-                            <div class="relative" x-cloak x-show.transition.origin.top="tooltip">
-                                <div class="absolute top-0 z-10 w-56 p-2 -mt-1 text-sm leading-tight text-white transform -translate-x-1/2 -translate-y-full bg-orange-500 rounded-lg shadow-lg">
-                                    Writing Sample One  {{ $student->first_writing ? 'Complete' : '' }}
-                                </div>
-                                <svg class="absolute z-10 w-6 h-6 text-orange-500 transform -translate-x-12 -translate-y-3 fill-current stroke-current" width="8" height="8">
-                                    <rect x="12" y="-10" width="8" height="8" transform="rotate(45)" />
-                                </svg>
-                            </div>
                         </div>
                     @endif
                     @if($student->second_writing)
-                        <div x-data="{ tooltip: false }" class="relative inline-flex">
-                            <button x-on:mouseover="tooltip = true" x-on:mouseleave="tooltip = false" class="flex-shrink-0 pr-2">
+                        <div class="relative inline-flex">
+                            <button class="flex-shrink-0 pr-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-green-600" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12l2 2 4-4M7.835 4.697a3.42 3.42 0 001.946-.806 3.42 3.42 0 014.438 0 3.42 3.42 0 001.946.806 3.42 3.42 0 013.138 3.138 3.42 3.42 0 00.806 1.946 3.42 3.42 0 010 4.438 3.42 3.42 0 00-.806 1.946 3.42 3.42 0 01-3.138 3.138 3.42 3.42 0 00-1.946.806 3.42 3.42 0 01-4.438 0 3.42 3.42 0 00-1.946-.806 3.42 3.42 0 01-3.138-3.138 3.42 3.42 0 00-.806-1.946 3.42 3.42 0 010-4.438 3.42 3.42 0 00.806-1.946 3.42 3.42 0 013.138-3.138z" />
                                 </svg>
                             </button>
-                            <div class="relative" x-cloak x-show.transition.origin.top="tooltip">
-                                <div class="absolute top-0 z-10 w-40 p-2 -mt-1 text-sm leading-tight text-white transform -translate-x-1/2 -translate-y-full bg-orange-500 rounded-lg shadow-lg">
-                                    Writing Sample Two  {{ $student->second_writing ? 'Complete' : '' }}
-                                </div>
-                                <svg class="absolute z-10 w-6 h-6 text-orange-500 transform -translate-x-12 -translate-y-3 fill-current stroke-current" width="8" height="8">
-                                    <rect x="12" y="-10" width="8" height="8" transform="rotate(45)" />
-                                </svg>
-                            </div>
                         </div>
                     @else
-                        <div x-data="{ tooltip: false }" class="relative inline-flex">
-                            <button onclick="Livewire.emit('openModal', 'writing-sample-modal')" x-on:mouseover="tooltip = true" x-on:mouseleave="tooltip = false" class="flex-shrink-0 pr-2">
+                        <div class="relative inline-flex">
+                            <button class="flex-shrink-0 pr-2">
                                 <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 {{ $student->second_writing ? 'text-green-400' : 'text-gray-400' }}" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 12h6m-6 4h6m2 5H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z" />
                                 </svg>
                             </button>
-                            <div class="relative" x-cloak x-show.transition.origin.top="tooltip">
-                                <div class="absolute top-0 z-10 w-40 p-2 -mt-1 text-sm leading-tight text-white transform -translate-x-1/2 -translate-y-full bg-orange-500 rounded-lg shadow-lg">
-                                    Writing Sample Two  {{ $student->second_writing ? 'Complete' : '' }}
-                                </div>
-                                <svg class="absolute z-10 w-6 h-6 text-orange-500 transform -translate-x-12 -translate-y-3 fill-current stroke-current" width="8" height="8">
-                                    <rect x="12" y="-10" width="8" height="8" transform="rotate(45)" />
-                                </svg>
-                            </div>
                         </div>
                     @endif
                 </div>
